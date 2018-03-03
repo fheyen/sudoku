69a4d98dced79a28710da9d11f78db30a0289483
fheyen <frank.heyen@outlook.de>
2018-03-03 18:42:49 +0100

    rewrote sudoku generator to be iterative instead of recursive to allow for bigger sudokus

--- sudoku.py
+++ sudoku.py
@@ -10,28 +10,29 @@ from __future__ import print_function
 import sys
 import math
 import random
+import time
 
 
-def creatematrix(w, h, initialval):
-    matrix = [[initialval for x in range(w)] for y in range(h)]
-    return matrix
+def create2dimarray(w, h, initialval):
+    sudoku = [[initialval for x in range(w)] for y in range(h)]
+    return sudoku
 
 
 def createemptysudoku(size):
-    return creatematrix(size, size, size)
+    return create2dimarray(size, size, size)
 
 
-def printmatrix(matrix):
+def printsudoku(sudoku):
     """
-    Prints a n x n sudoku matrix with grid lines
+    Prints a n x n sudoku sudoku with grid lines
     """
-    size = len(matrix)
+    size = len(sudoku)
     blocksize = int(math.sqrt(size))
     lookup = "0123456789abcdefghijklmnopqrstuvwxyz"
     output = ""
     row_num = 0
     output += "-" * (1 + (blocksize*3+3)*blocksize) + "\n"
-    for row in matrix:
+    for row in sudoku:
         output += "|  "
         col_num = 0
         for value in row:
@@ -50,13 +51,13 @@ def printmatrix(matrix):
     print(output)
 
 
-def checkrows(matrix):
+def checkrows(sudoku):
     """
     Checks if each row contains each value only once
     """
-    size = len(matrix)
+    size = len(sudoku)
     row_num = 0
-    for row in matrix:
+    for row in sudoku:
         numbercontained = [False for x in range(size)]
         for value in row:
             # if placeholder, ignore it
@@ -69,15 +70,15 @@ def checkrows(matrix):
     return True
 
 
-def checkcols(matrix):
+def checkcols(sudoku):
     """
     Checks if each column contains each value only once
     """
-    size = len(matrix)
+    size = len(sudoku)
     for col_num in range(size):
         numbercontained = [False for x in range(size)]
         for row_num in range(size):
-            value = matrix[row_num][col_num]
+            value = sudoku[row_num][col_num]
             # if placeholder, ignore it
             if value in range(size):
                 if numbercontained[value]:
@@ -87,18 +88,18 @@ def checkcols(matrix):
     return True
 
 
-def checkblocks(matrix):
+def checkblocks(sudoku):
     """
     Checks if each block contains each value only once
     """
-    size = len(matrix)
+    size = len(sudoku)
     blocksize = int(math.sqrt(size))
     row_num = 0
     col_num = 0
     for block_num in range(size):
         numbercontained = [False for x in range(size)]
         for value_num in range(size):
-            value = matrix[row_num][col_num]
+            value = sudoku[row_num][col_num]
             # if placeholder, ignore it
             if value in range(size):
                 if numbercontained[value]:
@@ -117,33 +118,33 @@ def checkblocks(matrix):
     return True
 
 
-def checksudoku(matrix):
+def checksudoku(sudoku):
     """
-    Checks if matrix contains a valid (partially) filled out sudoku
+    Checks if sudoku contains a valid (partially) filled out sudoku
     """
-    if checkrows(matrix):
-        if checkcols(matrix):
-            if checkblocks(matrix):
+    if checkrows(sudoku):
+        if checkcols(sudoku):
+            if checkblocks(sudoku):
                 return True
     return False
 
 
-def trysudoku(matrix, row, col, value):
+def trysudoku(sudoku, row, col, value):
     """
-    Tries to insert value to matrix[row][col] and
+    Tries to insert value to sudoku[row][col] and
     checks if it is a valid sudoku. If yes, the
-    new matrix is returned, if not the change is
-    undone and the old matrix is returned.
+    new sudoku is returned, if not the change is
+    undone and the old sudoku is returned.
     """
-    oldvalue = matrix[row][col]
-    matrix[row][col] = value
-    if checksudoku(matrix):
+    oldvalue = sudoku[row][col]
+    sudoku[row][col] = value
+    if checksudoku(sudoku):
         # keep change
-        return matrix, True
+        return sudoku, True
     else:
         # undo change
-        matrix[row][col] = oldvalue
-        return matrix, False
+        sudoku[row][col] = oldvalue
+        return sudoku, False
 
 
 def getrandomarray(size):
@@ -152,91 +153,149 @@ def getrandomarray(size):
     return array
 
 
-def recurse(matrix, row, col, depth):
-    size = len(matrix)
-
-    col_new = col + 1
-    row_new = row
-    if col_new == size:
-        col_new = 0
-        row_new = row + 1
-
-    # randomize value order
-    array = getrandomarray(size)
-    for value in array:
-        # test if valid
-        matrix, success = trysudoku(matrix, row, col, value)
-        if success:
-            # check if sudoku is filled out completely
-            if row == size - 1 and col == size - 1:
-                return matrix, True
-            else:
-                # if not finished, recurse deeper
-                matrix, finished = recurse(
-                    matrix, row_new, col_new, depth+1)
-                if finished:
-                    # pass on result
-                    return matrix, True
-
-    # no success? return and try other path
-    return matrix, False
+def getnextfield(row, col, size):
+    """
+    Returns coordinates of next field
+    """
+    col = col + 1
+    if col >= size:
+        col = 0
+        row = row + 1
+    return row, col
 
 
-def generatesudoku(size):
+def getpreviousfield(row, col, size):
+    """
+    Returns coordinates of previous field
+    """
+    col = col - 1
+    if col <= -1:
+        col = size - 1
+        row = row - 1
+    return row, col
+
+
+def generatesudoku_iterative(size):
     """
     Generates a fully filled out sudoku with
     the specified size.
 
     Tree search with randomized value choice.
     """
+    sudoku = createemptysudoku(size)
+    # keep track of shuffled values current index
+    # for each field in the sudoku
+    value_index_tuples = create2dimarray(size, size, None)
+
+    # only create shuffled values once
+    for r in range(size):
+        for c in range(size):
+            value_index_tuples[r][c] = (getrandomarray(size), 0)
+
+    # fill out sudoku field by field and
+    # go back one field if failed
+    row = 0
+    col = 0
+    num_tests = 0
+    print("number of tests:")
+    while(True):
+        index = value_index_tuples[row][col][1]
+
+        if index >= size:
+            # all values failed, reset current field
+            # and go back to previous field
+            sudoku[row][col] = size
+            row, col = getpreviousfield(row, col, size)
+            # check if failed completely
+            if row <= -1:
+                print(num_tests)
+                return sudoku, False
+            # increase index of previous field
+            value_index_tuples[row][col] = (
+                value_index_tuples[row][col][0],
+                value_index_tuples[row][col][1] + 1)
+        else:
+            # show performance message
+            num_tests += 1
+            if num_tests % 1000 == 0:
+                print(num_tests, end="\r")
+
+            # test next value
+            value = value_index_tuples[row][col][0][index]
+            sudoku, success = trysudoku(sudoku, row, col, value)
+
+            if success:
+                # go to next field
+                row, col = getnextfield(row, col, size)
+                # check if finished
+                if row >= size:
+                    print(num_tests)
+                    return sudoku, True
+                else:
+                    # reset index of next field
+                    value_index_tuples[row][col] = (
+                        value_index_tuples[row][col][0],
+                        0)
+            else:
+                # increase index of current field
+                value_index_tuples[row][col] = (
+                    value_index_tuples[row][col][0],
+                    value_index_tuples[row][col][1] + 1)
+
+    print(num_tests)
+    return sudoku, False
+
+
+def generategaps(sudoku, gap_probability):
+    """
+    Adds gaps to a filled out sudoku
+    """
+    size = len(sudoku)
+    for row in range(size):
+        for col in range(size):
+            if random.uniform(0, 1) < gap_probability:
+                sudoku[row][col] = size
+    return sudoku
+
+
+def solvesudoku(sudoku):
+    """
+    Solves a partially filled out sudoku
+    """
+    # TODO:
+
+
+def main(size=9, gap_probability=0.5):
+    """
+    Reads console arguments and runs generator
+    """
+    # read arguments
+    if len(sys.argv) == 3:
+        size = int(sys.argv[1])
+        gap_probability = float(sys.argv[2])
+    else:
+        print("possible arguments:")
+        print("- sudoku size (try 1, 4, 9, 16 or 25)")
+        print("- gap probability in [0.0, 1.0]")
+
     # check if size is square number
     if not (math.sqrt(size)).is_integer():
         print("{0} is not a square number!".format(size))
         sys.exit(1)
 
-    matrix = createemptysudoku(size)
-    matrix, finished = recurse(matrix, 0, 0, 0)
-    return matrix, finished
-
-
-def generategaps(matrix, gap_probability):
-    size = len(matrix)
-    for row in range(size):
-        for col in range(size):
-            if random.uniform(0, 1) < gap_probability:
-                matrix[row][col] = size
-    return matrix
-
-
-def main(size=9, gap_probability=0.5, max_tries=50):
-
-    if len(sys.argv) == 4:
-        size = int(sys.argv[1])
-        gap_probability = float(sys.argv[2])
-        max_tries = int(sys.argv[3])
-    else:
-        print("possible arguments:")
-        print("- sudoku size (try 4, 9, 16, 25)")
-        print("- gap probability in [0, 1]")
-        print("- max number of tries")
-
     print("\nsudoku size: ", size)
 
-    finished = False
-    tries = 1
+    # try until finished or too many tries
+    start = time.time()
+    result, finished = generatesudoku_iterative(size)
+    print("\ntime: {0:3.1f}s".format(time.time() - start))
 
-    while not finished and tries <= max_tries:
-        result, finished = generatesudoku(size)
-        print("try {0} of {1}, finished: {2}".format(
-            tries, max_tries, finished))
-
-        if finished or tries == max_tries:
-            printmatrix(result)
+    if finished:
+        printsudoku(result)
+        if finished:
             print("adding gaps with probability", gap_probability)
-            matrix = generategaps(result, gap_probability)
-            printmatrix(matrix)
-
-        tries += 1
+            sudoku = generategaps(result, gap_probability)
+            printsudoku(sudoku)
 
     return result
 
